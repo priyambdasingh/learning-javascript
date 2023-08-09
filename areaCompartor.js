26e792a2926b8809fb8eb21fb4afc3306ac5250e
priyambdasingh <yashiparihar01@gmail.com>
2023-08-09 23:46:20 +0530

    new comment added

--- areaCompartor.js
+++ areaCompartor.js
@@ -1,4 +1,4 @@
-
+//this is new branch
 // 2. Write a function 'areaCompartor' which takes 2 number as input which are sides of square and returns the area of square that has more area.
 // Ex.
 // Input: areaCompartor(4,5)
@@ -6,6 +6,7 @@
 
 // Input: areaCompartor(6,5)
 // Output: 36
+// this 
 function areaCompartor(side1,side2){
 var area1 = side1 **2;
 var area2 = side2 **2;
