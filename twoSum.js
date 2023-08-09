29a7c44bca9225a7ae7feea4d91ce728f56044d1
priyambdasingh <140975527+priyambdasingh@users.noreply.github.com>
2023-08-09 12:24:55 +0530

    Update twoSum.js

--- twoSum.js
+++ twoSum.js
@@ -1,1 +1,12 @@
- # learning-javascript
+var twoSum = function(nums, target) {
+  for (var i = 0; i < nums. length; i++) {
+      for (var j = i + 1; j < nums. length; j++) {
+         if(nums [i] + nums[j] === target) {
+         return [i, j];
+         }
+      }
+  }
+};
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1]
