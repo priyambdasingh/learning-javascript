6f5b60da65f99bdd9c3cc817db0e47c033b75dc5
priyambdasingh <yashiparihar01@gmail.com>
2023-08-10 11:18:07 +0530

    new comment added

---
+++ code3.js
@@ -0,0 +1,17 @@
+// 217. Contains Duplicate
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+// Output: true
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+// Output: false
+var containsDuplicate = function(nums) {
+    const map = {}
+    for(const num of nums) {
+         if(map[num]) return true
+        map[num] = true
+    }
+    return false
+};
